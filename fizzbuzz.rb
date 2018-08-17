a405edef1f048935559bb6c3dd7f25c1212b0bae
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-17 19:02:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
+  if num % 3 == 0
+    "Fizz"
+  if num % 5 == 0
+    "Buzz"
 end
